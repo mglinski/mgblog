f6ede5210cd2a69970789f439e1572681ab8627a
Matthew Glinski <matthewglinski@gmail.com>
2015-05-28 15:33:14 -0400

    Update site-footer.blade.php

--- resources/views/partials/site-footer.blade.php
+++ resources/views/partials/site-footer.blade.php
@@ -2,8 +2,10 @@
 	<div class="container">
 		<div class="row">
 			<p class="pull-right">
+				<!--
 				<a href="<?=URL::route('about')?>"><i class="fa fa-info-circle"></i> About</a>
 				&nbsp;
+				-->
 				<a href="https://github.com/mglinski/mgblog"><i class="fa fa-github-square"></i> Source Code</a>
 			</p>
 			<p>&copy; {{{ date('Y') }}} - Matthew Glinski</p>
